562eb1958d7b657a851b078cb14e04f37fad05fa
dpratik23 <d.pratik23@gmail.com>
2025-05-30 21:52:47 +0530

    Update window.js

--- window.js
+++ window.js
@@ -1,10 +1,7 @@
-let goingDown = true;
-let interval = setInterval(() => {
-    if (goingDown) {
-        window.scrollBy(0, 50);
-        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) goingDown = false;
-    } else {
-        window.scrollBy(0, -50);
-        if (window.scrollY <= 0) goingDown = true;
-    }
-}, 3000);
+javascript:(function(){
+  if(window.scrollLoop) { clearInterval(window.scrollLoop); window.scrollLoop = null; alert("Auto-scroll stopped"); }
+  else {
+    window.scrollLoop = setInterval(function(){ window.scrollBy(0, 50); }, 3000);
+    alert("Auto-scroll started");
+  }
+})();
